7f7a17fc6ebc0b92d9419986677452acd15495b8
sanobar28 <sanobar.mujawar@gmail.com>
2021-05-18 15:42:53 +0530

    [Sanobar] Doublet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -28,48 +28,5 @@ percentage[T]=$(($TAILS*10))
 echo "percentage output "${percentage[@]}
 
 
-declare -A flipcoin
-count=1
-HH=0
-HT=0
-TH=0
-TT=0
-max=10
-while [[ $count -le $max ]]
-do
-   flip1=$((RANDOM%2))
-   flip2=$((RANDOM%2))
-
-   if [[ $flip1 -eq 1 && $flip2 -eq 1 ]]
-   then
-      ((HH++))
-   elif [[ $flip1 -eq 0 && $flip2 -eq 0 ]]
-   then
-      ((TT++))
-   elif [[ $flip1 -eq 0 && $flip2 -eq 1 ]]
-   then
-      ((TH++))
-   else
-      ((HT++))
-fi
-   flipcoin["$count"]=$flip1$flip2
-   ((count++))
-done
-
-
-echo "FLIPCOUNT doublet -"$count
-echo "HH Percentage -"$(($HH*10))
-echo "TT Percentage -"$(($TT*10))
-echo "HT Percentage -"$(($HT*10))
-echo "TH Percentage -"$(($TH*10))
-echo "flipcoin output "${flipcoin[@]}
-percentage[HH]=$(($HH*10))
-percentage[TT]=$(($TT*10))
-percentage[HT]=$(($HT*10))
-percentage[TH]=$(($TH*10))
-
-echo "percentage output "${percentage[@]}
-
-
 
 
